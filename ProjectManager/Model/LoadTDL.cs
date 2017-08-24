67107ba2ec234bf2dc77e3838a6764b4f5b5e1da
James Rudy <rudy523@outlook.com>
2017-08-24 12:58:23 -0400

    Added Test Console Project, began TDL tracking feature

---
+++ ProjectManager/Model/LoadTDL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+
+namespace ProjectManager.Model
+{
+    public class LoadTDL
+    {
+        #region Properties
+        // Collection holding all TDL data from source File
+        public IEnumerable<XElement> RawTDLs { get; set; }
+
+
+
+        #endregion
+        #region Constructors
+        // This constructor gets string input from the TDL search bar and uses it to filter RawTDLs
+        public LoadTDL(string[] input)
+        {
+
+
+            string FinancialFeed = @"\\Scrdata\bah\Rudy\Workbook Development\Programming\Financial Workbook TDLs.xml";
+            XElement Root = XElement.Load(FinancialFeed);
+
+
+        }
+        #endregion
+        #region Methods
+        private void LoadSourceData()
+        {
+            // Sets location of source data file
+            string FinancialFeed = @"\\Scrdata\bah\Rudy\Workbook Development\Programming\Financial Workbook TDLs.xml";
+            // Loads source data into XElement. This will be the source to run queries against. NOTE: May require conversion to IEnumberable.
+            XElement Root = XElement.Load(FinancialFeed);
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+using System.Collections.ObjectModel;
+
+
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FinancialFeed = @"\\Scrdata\bah\Rudy\Workbook Development\Programming\Financial Workbook TDLs.xml";
+
+            XElement FinanceRoot = XElement.Load(FinancialFeed);
+            IEnumerable <XElement> children = FinanceRoot.Descendants();
+            IEnumerable<XAttribute> atts = children.ElementAt(5).Attributes();
+            Console.WriteLine(FinanceRoot);
+            //Console.WriteLine(FinanceRoot);
+            Console.ReadLine();
+
+    }
+        }
+}
